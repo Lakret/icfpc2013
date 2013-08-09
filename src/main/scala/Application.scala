210db8732e5b157db6f78b473216aa0c086289e0
Lakret <Rakuretu@gmail.com>
2013-08-09 21:48:43 +0600

    merge

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,48 +1,19 @@
-import akka.io.IO
-import akka.pattern.ask
-import akka.actor.ActorSystem
-import akka.event.Logging
-import com.typesafe.config.ConfigFactory
-import scala.concurrent.duration._
-import scala.util.Success
-import spray.can.Http
-import spray.client.pipelining._
-import spray.http._
-import spray.http.Uri.Query
-import spray.httpx.SprayJsonSupport
-import spray.util._
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.util.Try
 
 object Application {
-  def main(arg : Array[String]) {
-    println("do nothing")
+  implicit val ec = ApiClient.system.dispatcher
+
+  def main(args: Array[String]) = {
+    /*
+    val problemsFuture = ApiClient.Problems(Some("problems.json"))
+    val problems = Await.result(problemsFuture, 5.second)
+    */
+    ApiClient.Training(TrainRequest(size = Some(3))) onSuccess   {
+      case train => {
+        println(train)
+      }
+    }
   }
-  // val config = ConfigFactory.load()
-  // val authToken = config.getString("auth.token")
-  // val host = config.getString("host")
-
-  // implicit val system = ActorSystem("kontur-ukko-icfpc2013")
-  // val log = Logging(system, getClass)
-  // import system.dispatcher
-  // import SprayJsonSupport._
-  // val pipeline = sendReceive ~> unmarshal[TrainingProblem]
-
-
-  // def buildUrl(path: String) = {
-  //   Uri.from(scheme = "http", host = host, path = "/" + path, query = Query("auth" -> authToken))
-  // }
-  // def main(arg: Array[String]) {
-  //   val trainingUrl = buildUrl("train")
-  //   val request = TrainRequest(size = Some(3), operators = None)
-  //   val response = pipeline(Post(trainingUrl, request))
-  //   response.onComplete {
-  //     case Success(r) =>
-  //       log.info("response '{}'", r)
-  //       shutdown()
-  //   }
-  // }
-
-  // def shutdown(): Unit = {
-  //   IO(Http).ask(Http.CloseAll)(1.second).await
-  //   system.shutdown()
-  // }
 }
